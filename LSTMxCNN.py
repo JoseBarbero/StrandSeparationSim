36d71d1fe0d4d2a49832882bb6704ecc8d5bfc7d
JoseBarbero <jabarbero@ubu.es>
2021-01-21 19:24:59 +0100

    CNNxLSTM

--- LSTMxCNN.py
+++ LSTMxCNN.py
@@ -110,17 +110,19 @@ if __name__ == "__main__":
             reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, verbose=1, min_delta=1e-4, mode='min')
 
             X_train_lstm = X_train[:,1,:,5:9]
-            #X_train_lstm = X_train_lstm.reshape((X_train_lstm.shape[0], 1, X_train_lstm.shape[1], X_train_lstm.shape[2]))
             X_train_cnn = X_train[:,:,:,:5]
             print(X_train_lstm.shape)
             print(X_train_cnn.shape)
-            history = model.fit([X_train_lstm, X_train_cnn], y_train,
-                                shuffle=True,
-                                batch_size=32,
-                                epochs=100,
-                                verbose=True,
-                                validation_data=(X_val, y_val),
-                                callbacks=[early_stopping_monitor, reduce_lr_loss])
+            model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+            # history = model.fit([X_train_lstm, X_train_cnn], y_train,
+            #                     shuffle=True,
+            #                     batch_size=32,
+            #                     epochs=100,
+            #                     verbose=True,
+            #                     validation_data=(X_val, y_val),
+            #                     callbacks=[early_stopping_monitor, reduce_lr_loss])
             print("Train results:")
             test_results(X_train, y_train, model)
             print("Test results:")
