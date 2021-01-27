a9c339d73fc1b285ec5f7c7c5403fea107b32cbc
JoseBarbero <jabarbero@ubu.es>
2021-01-27 12:53:32 +0100

    lstxlstm

--- LSTMxLSTM.py
+++ LSTMxLSTM.py
@@ -140,13 +140,11 @@ if __name__ == "__main__":
 
             for layer in model.layers:
                 print(layer.get_config())
-            early_stopping_monitor = EarlyStopping( monitor='val_loss', min_delta=0, patience=10, 
+            early_stopping_monitor = EarlyStopping( monitor='val_loss', min_delta=0, patience=6, 
                                                     verbose=1, mode='min', baseline=None,
                                                     restore_best_weights=True)
             reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, verbose=1, min_delta=1e-4, mode='min')
 
-
-            
             history = model.fit([X_train_seq, X_train_opn], y_train,
                                 shuffle=True,
                                 batch_size=128,
