4b684a17631b2f52d5e3667702b98ce8d9cbbb2e
JoseBarbero <jabarbero@ubu.es>
2021-02-03 20:01:10 +0100

    transformer

---
+++ Transformer.py
@@ -0,0 +1,139 @@
+"""
+Title: Text classification with Transformer
+Author: [Apoorv Nandan](https://twitter.com/NandanApoorv)
+Date created: 2020/05/10
+Last modified: 2020/05/10
+Description: Implement a Transformer block as a Keras layer and use it for text classification.
+"""
+"""
+## Setup
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+"""
+## Implement a Transformer block as a layer
+"""
+
+
+class TransformerBlock(layers.Layer):
+    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
+        super(TransformerBlock, self).__init__()
+        self.att = layers.MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+        self.ffn = keras.Sequential(
+            [layers.Dense(ff_dim, activation="relu"), layers.Dense(embed_dim),]
+        )
+        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = layers.Dropout(rate)
+        self.dropout2 = layers.Dropout(rate)
+
+    def call(self, inputs, training):
+        attn_output = self.att(inputs, inputs)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(inputs + attn_output)
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        return self.layernorm2(out1 + ffn_output)
+
+
+def transformer1():
+
+    model = tf.keras.models.Sequential()
+    model.add(TransformerBlock(8, 4, 32))
+    model.add(GlobalAveragePooling1D())
+    model.add(Dropout(0.2))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=["accuracy", "AUC"])
+
+    return model
+
+
+def transformer2():
+
+    inputs = layers.Input(shape=(200,8))
+    transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim)
+    x = transformer_block(inputs)
+    x = layers.GlobalAveragePooling1D()(x)
+    x = layers.Dropout(0.1)(x)
+    x = layers.Dense(20, activation="relu")(x)
+    x = layers.Dropout(0.1)(x)
+    outputs = layers.Dense(1, activation="sigmoid")(x)
+
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    return model
+
+if __name__ == "__main__":
+    seed = 42
+    np.random.seed(seed)
+
+    X_train_file = open('../data/serialized/X_train_channels_onehot_noAA.pkl', 'rb')
+    y_train_file = open('../data/serialized/y_train_channels_onehot_noAA.pkl', 'rb')
+    X_val_file = open('../data/serialized/X_val_channels_onehot_noAA.pkl', 'rb')
+    y_val_file = open('../data/serialized/y_val_channels_onehot_noAA.pkl', 'rb')
+    X_test_file = open('../data/serialized/X_test_channels_onehot_noAA.pkl', 'rb')
+    y_test_file = open('../data/serialized/y_test_channels_onehot_noAA.pkl', 'rb')
+
+    X_train = pickle.load(X_train_file)
+    y_train = pickle.load(y_train_file)
+    X_val = pickle.load(X_val_file)
+    y_val = pickle.load(y_val_file)
+    X_test = pickle.load(X_test_file)
+    y_test = pickle.load(y_test_file)
+
+    X_train_file.close()
+    y_train_file.close()
+    X_val_file.close()
+    y_val_file.close()
+    X_test_file.close()
+    y_test_file.close()
+
+    X_train = X_train[:,1,:,5:13]
+    X_val = X_val[:,1,:,5:13]
+    X_test = X_test[:,1,:,5:13]
+    
+    if len(sys.argv) < 2:
+        run_id = str(datetime.now()).replace(" ", "_").replace("-", "_").replace(":", "_").split(".")[0]
+    else:
+        run_id = sys.argv[1]
+        #run_id = "".join(categories)
+
+    log_file = "logs/"+run_id+".log"
+    hist_file = "logs/"+run_id+".pkl"
+    plot_file = "logs/"+run_id+".png"
+
+    model = transformer1()
+
+    with open(log_file, 'w') as f:
+        with redirect_stdout(f):
+            #model.summary()
+
+            for layer in model.layers:
+                print(layer.get_config())
+            early_stopping_monitor = EarlyStopping( monitor='val_loss', min_delta=0, patience=10, 
+                                                    verbose=1, mode='min', baseline=None,
+                                                    restore_best_weights=True)
+            reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, verbose=1, min_delta=1e-4, mode='min')
+
+            history = model.fit(X_train, y_train,
+                                shuffle=True,
+                                batch_size=32,
+                                epochs=50,
+                                verbose=True,
+                                validation_data=(X_val, y_val),
+                                callbacks=[early_stopping_monitor, reduce_lr_loss])
+            print("Train results:")
+            test_results(X_train, y_train, model)
+            print("Test results:")
+            test_results(X_test, y_test, model)
+
+    with open(hist_file, 'wb') as file_pi:
+        pickle.dump(history.history, file_pi)
+
+    plot_train_history(history.history, plot_file)
