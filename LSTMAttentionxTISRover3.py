d9cb18d41b6b49a6406c975c28522592d8796a9b
JoseBarbero <jabarbero@ubu.es>
2021-02-11 10:41:18 +0100

    LSTMAttxCNN

--- LSTMAttentionxTISRover3.py
+++ LSTMAttentionxTISRover3.py
@@ -1,39 +1,25 @@
 import numpy as np
 import re
 import os
-import autokeras as ak
 import pandas as pd
-import matplotlib.pyplot as plt
 import sys
 import pickle
-import keras
-import tensorflow as tf
-from Attention import Attention
-from keras.models import Sequential, Model
-from keras.callbacks import EarlyStopping, ReduceLROnPlateau
-from keras.layers import Dense, Activation, Dropout, Flatten, Conv1D, Conv2D, MaxPooling1D, MaxPooling2D, AveragePooling2D, LayerNormalization, Bidirectional
-from keras.layers import Conv3D, MaxPooling3D, AveragePooling3D
-from keras.layers import LSTM
-from keras.layers import concatenate
 from ReadData import read_data_as_img, read_data_structured, read_data_st, seq_to_array, seq_to_onehot_array
-from Preprocessing import ros, smote, adasyn
 from Results import report_results_imagedata, make_spider_by_temp, report_results_st, test_results, plot_train_history
-from keras import backend as K
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import load_model
-from keras import Sequential
-from sklearn.metrics import f1_score, roc_auc_score, accuracy_score, log_loss, fowlkes_mallows_score, cohen_kappa_score, precision_score, recall_score
 from datetime import datetime
 from contextlib import redirect_stdout
-from keras.layers.embeddings import Embedding
-from keras.preprocessing import sequence
+import keras
+from keras import Sequential, Model
+from keras.layers import Bidirectional, LSTM, Dropout, Flatten, Dense, Conv2D, MaxPooling2D, Conv1D, concatenate
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from keras_self_attention import SeqSelfAttention
 
 
 def lstmattxtisrover3():
     seq = Sequential()
     
-    seq.add(Bidirectional(LSTM(units=64, return_sequences=True, dropout=0.3, input_shape=(200, 8))))
+    #seq.add(Bidirectional(LSTM(units=64, return_sequences=True, dropout=0.3, input_shape=(200, 8))))
+    seq.add(LSTM(units=64, return_sequences=True, dropout=0.3, input_shape=(200, 8)))
     seq.add(Dropout(0.75))
     seq.add(SeqSelfAttention(units=64, attention_activation='sigmoid'))
     seq.add(Dropout(0.75))
@@ -124,13 +110,10 @@ if __name__ == "__main__":
     plot_file = "logs/"+run_id+".png"
 
     model = lstmattxtisrover3()
-    model.build([X_train_seq.shape, X_train_cnn.shape])
-    model.summary()
     
     with open(log_file, 'w') as f:
         with redirect_stdout(f):
-            model.summary()
-
+            
             for layer in model.layers:
                 print(layer.get_config())
             early_stopping_monitor = EarlyStopping( monitor='val_loss', min_delta=0, patience=10, 
