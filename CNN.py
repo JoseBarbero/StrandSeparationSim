d9ec8c59b5331f805f0c783242d801cbd1446c7f
JoseBarbero <jabarbero@ubu.es>
2021-10-09 17:04:15 +0200

    CNN multigpu

--- CNN.py
+++ CNN.py
@@ -95,12 +95,12 @@ if __name__ == "__main__":
         seed = 42
         np.random.seed(seed)
 
-        X_train_file = open('../databubbles/serialized/X_train_onlybub10.pkl', 'rb')
-        y_train_file = open('../databubbles/serialized/y_train_onlybub10.pkl', 'rb')
-        X_val_file = open('../databubbles/serialized/X_val_onlybub10.pkl', 'rb')
-        y_val_file = open('../databubbles/serialized/y_val_onlybub10.pkl', 'rb')
-        X_test_file = open('../databubbles/serialized/X_test_onlybub10.pkl', 'rb')
-        y_test_file = open('../databubbles/serialized/y_test_onlybub10.pkl', 'rb')
+        X_train_file = open('../databubbles/serialized/X_train_onlyseq.pkl', 'rb')
+        y_train_file = open('../databubbles/serialized/y_train_onlyseq.pkl', 'rb')
+        X_val_file = open('../databubbles/serialized/X_val_onlyseq.pkl', 'rb')
+        y_val_file = open('../databubbles/serialized/y_val_onlyseq.pkl', 'rb')
+        X_test_file = open('../databubbles/serialized/X_test_onlyseq.pkl', 'rb')
+        y_test_file = open('../databubbles/serialized/y_test_onlyseq.pkl', 'rb')
 
         X_train = pickle.load(X_train_file)
         X_train = np.reshape(X_train, (*X_train.shape, 1))
