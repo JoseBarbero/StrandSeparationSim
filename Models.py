a436e5f73c717f21907baee422c0ab63d768cb65
JoseBarbero <jabarbero@ubu.es>
2021-10-14 12:23:19 +0200

    lstm

--- Models.py
+++ Models.py
@@ -20,6 +20,20 @@ def lstm_att(inputshape):
     sequence_input = tf.keras.layers.Input(shape=inputshape)
     
     x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=64, return_sequences=True, dropout=0.3, input_shape=inputshape))(sequence_input)
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dense(units=64, activation='relu')(x)
+    x = tf.keras.layers.Dropout(0.75)(x)
+    output = tf.keras.layers.Dense(1)(x)
+    output = tf.keras.layers.Activation('sigmoid')(output)
+
+    model = tf.keras.Model(inputs=sequence_input, outputs=output)
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=["accuracy", 'AUC'])
+
+def lstm_att(inputshape):
+
+    sequence_input = tf.keras.layers.Input(shape=inputshape)
+    
+    x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=64, return_sequences=True, dropout=0.3, input_shape=inputshape))(sequence_input)
     x = tf.keras.layers.MultiHeadAttention(num_heads=2, key_dim=2, attention_axes=(1,2))(x, x)
     x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=64, return_sequences=True, dropout=0.3,))(x)
     x = tf.keras.layers.Flatten()(x)
@@ -36,21 +50,14 @@ def lstm_att(inputshape):
 def cnn(inputshape):
     model = Sequential()
 
-    #model.add(Conv2D(filters=32, kernel_size=3, data_format='channels_last', strides=1, activation='relu', input_shape=(28, 200, 1)))
-    #model.add(Conv2D(filters=32, kernel_size=3, data_format='channels_last', strides=1, activation='relu', input_shape=(200, 4)))
-    model.add(Conv1D(filters=32, kernel_size=3, data_format='channels_last', strides=1, activation='relu', input_shape=inputshape))
+    model.add(Conv2D(filters=32, kernel_size=3, data_format='channels_last', strides=1, activation='relu', input_shape=(28, 200, 1)))
+    model.add(Conv2D(filters=32, kernel_size=3, data_format='channels_last', strides=1, activation='relu', input_shape=(200, 4)))
     
-    #model.add(MaxPooling2D(2))
-
-    #model.add(Conv2D(filters=32, kernel_size=3, strides=1, activation='relu'))
-    model.add(Conv1D(filters=32, kernel_size=3, strides=1, activation='relu'))
-    
-    #model.add(MaxPooling2D(2))
-
-    #model.add(Conv2D(filters=32, kernel_size=3, strides=1, activation='relu'))
-    model.add(Conv1D(filters=32, kernel_size=3, strides=1, activation='relu'))
-    
-    #model.add(MaxPooling2D(3))
+    model.add(MaxPooling2D(2))
+    model.add(Conv2D(filters=32, kernel_size=3, strides=1, activation='relu'))
+    model.add(MaxPooling2D(2))
+    model.add(Conv2D(filters=32, kernel_size=3, strides=1, activation='relu'))
+    model.add(MaxPooling2D(3))
 
     model.add(Flatten())
 
