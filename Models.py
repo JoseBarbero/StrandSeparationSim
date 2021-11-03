3a702a476f25f72de9fb9e3243127a0ef56b6c59
JoseBarbero <jabarbero@ubu.es>
2021-11-03 17:04:22 +0100

    lstmatt seq+bub

--- Models.py
+++ Models.py
@@ -51,7 +51,7 @@ def lstm_att(inputshape):
 
     sequence_input = tf.keras.layers.Input(shape=inputshape)
     
-    x = tf.keras.layers.MultiHeadAttention(num_heads=2, key_dim=2, attention_axes=(2))(sequence_input, sequence_input)
+    x = tf.keras.layers.MultiHeadAttention(num_heads=32, key_dim=2, attention_axes=(2))(sequence_input, sequence_input)
     x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=64, return_sequences=True, dropout=0.3,))(x)
     x = tf.keras.layers.Flatten()(x)
     x = tf.keras.layers.Dense(units=64, activation='relu')(x)
