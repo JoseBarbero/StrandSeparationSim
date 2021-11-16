311c0f28376503aafdb829e00f622c4b237d05b6
JoseBarbero <jabarbero@ubu.es>
2021-11-16 17:23:24 +0100

    cnnxlstm with attention all probs

--- Models.py
+++ Models.py
@@ -260,7 +260,7 @@ def cnnxlstm_allprobs(seqshape, opn_shape, bub8_shape, bub10_shape, bub12_shape,
     vrnorm_x = Dense(64, activation = 'relu')(vrnorm_x)
     vrnorm_out = Dropout(0.2)(vrnorm_x)
 
-    merged = Add()([lstm_out, bub8_out, bub10_out, bub12_out, vrnorm_out])
+    merged = Add()([seq_out, bub8_out, bub10_out, bub12_out, vrnorm_out])
     
     z = Dense(128, activation="relu")(merged)
     z = Dense(1, activation="sigmoid")(merged)
