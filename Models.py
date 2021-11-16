f49f85f556400aac0e6bafb5766b89f5c9ae8be8
JoseBarbero <jabarbero@ubu.es>
2021-11-16 17:04:40 +0100

    cnnxlstm with attention all probs

--- Models.py
+++ Models.py
@@ -167,7 +167,7 @@ def cnnxlstm(seqshape, probsshape):
 
 def cnnxlstm_allprobs(seqshape, opn_shape, bub8_shape, bub10_shape, bub12_shape, vrnorm_shape):
     
-    seq_in = Input(shape=seqpe)
+    seq_in = Input(shape=seqshape)
     seq_x = Bidirectional(LSTM(units=64, return_sequences=True, dropout=0.3, input_shape=seqshape))(seq_in)
     seq_x = MultiHeadAttention(num_heads=2, key_dim=2, attention_axes=(1,2))(seq_x, seq_x)
     seq_x = Bidirectional(LSTM(units=64, return_sequences=True, dropout=0.3,))(seq_x)
